85d43162ac13e59805c5024aefc7919f5a78226a
sebastianManco <sebastian.manco1997@gmail.com>
2020-07-07 04:36:38 -0500

    commit para cambio de rama

--- routes/web.php
+++ routes/web.php
@@ -20,5 +20,4 @@ Route::get('/', function () {
 Auth::routes(['verify' => true]);
 
 Route::get('/home', 'HomeController@index')->name('home')->middleware('verified');
-Route::get('/home/userList', 'UserController@index');
 Route::resource('users','UserController');
